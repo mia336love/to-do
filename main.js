f078a14b904c053cc939539f40be5666224ed9fc
mia336love <122865270+mia336love@users.noreply.github.com>
2023-03-31 19:12:57 +0300

    task buttons was added

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 let inp = document.querySelector(".inp");
 let btn = document.querySelector(".btn");
-// let task = document.getElementById("h2");
-let task = document.querySelector("ul");
+let taskList = document.querySelector(".taskList");
 
-let toDo = [];
-function addTask() {
-  task.textContent = inp.value;
-  toDo.push(task);
-}
+// let toDo = [];
+// function addTask() {
+//   task.textContent = inp.value;
+//   toDo.push(task);
+// }
 
-function display() {
-  toDo.forEach();
-}
-btn.addEventListener("click");
+// function display() {
+//   toDo.forEach();
+// }
+// btn.addEventListener("click");
 
 // use submit for delete btn
+
+function addTask() {
+  let task = inp.value;
+
+  let display = `
+    <li class="taskList">
+        <span class="spanTitle">${task}</span>
+        <div>
+          <button class="doneBtn">&#10004</button>
+          <button class="deleteBtn">&#10008</button>
+        </div>
+      </li>
+  `;
+  taskList.insertAdjacentHTML("beforeend", display);
+}
+btn.addEventListener("click", addTask);
+
+function taskButtons() {
+  doneBtn = document.querySelector(".doneBtn");
+  console.log(doneBtn);
+}
